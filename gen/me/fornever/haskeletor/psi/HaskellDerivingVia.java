19aa6cd35079627b4384c34139cf627d8f33a9c0
Friedrich von Never <friedrich@fornever.me>
2026-04-04 23:28:37 +0200

    Build: migrate to generated Haskell parser (not working yet)

--- gen/me/fornever/haskeletor/psi/HaskellDerivingVia.java
+++
@@ -1,6 +0,0 @@
-// This is a generated file. Not intended for manual editing.
-package me.fornever.haskeletor.psi;
-
-public interface HaskellDerivingVia extends HaskellCompositeElement {
-
-}
--- gen/me/fornever/haskeletor/psi/HaskellQuasiQuote.java
+++
@@ -1,8 +0,0 @@
-// This is a generated file. Not intended for manual editing.
-package me.fornever.haskeletor.psi;
-
-import com.intellij.psi.PsiLanguageInjectionHost;
-
-public interface HaskellQuasiQuote extends HaskellQuasiQuoteElement, PsiLanguageInjectionHost {
-
-}
